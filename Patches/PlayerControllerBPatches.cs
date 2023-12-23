f7cdc3320121a733c93a1d329fb1edd23f64b836
Ccode-lang <coop112097@gmail.com>
2023-12-23 15:36:06 -0700

    Add project files.

---
+++ Patches/PlayerControllerBPatches.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameNetcodeStuff;
+using HarmonyLib;
+
+namespace MoreOxygen.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB), "SetFaceUnderwaterFilters")]
+    internal class PlayerControllerBPatches
+    {
+        public static void Prefix()
+        {
+            StartOfRound.Instance.drowningTimer = 1f;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreOxygen
+{
+    [BepInPlugin(MyGuid, PluginName, VersionString)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string MyGuid = "Ccode.MoreOxygen";
+        private const string PluginName = "Ccode.MoreOxygen";
+        private const string VersionString = "0.0.1";
+
+        private static readonly Harmony Harmony = new Harmony(MyGuid);
+
+        public static ManualLogSource Log;
+
+        public void Awake()
+        {
+            Harmony.PatchAll();
+            Logger.LogInfo(PluginName + " " + VersionString + " " + "loaded.");
+            Log = Logger;
+        }
+    }
+}
